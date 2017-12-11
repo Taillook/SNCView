ef906ef68e43febc67b55a387067a4bb4bdb3bdc
Taillook <jousoku@gmail.com>
2017-12-11 18:21:44 +0900

    .gitignore is now working

--- Example/Pods/Target Support Files/Pods-SNCView_Tests/Pods-SNCView_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SNCView_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_SNCView_Tests
-@end
--- Example/Pods/Target Support Files/SNCView/SNCView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SNCView : NSObject
-@end
-@implementation PodsDummy_SNCView
-@end
